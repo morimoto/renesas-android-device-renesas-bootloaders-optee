cafa731e5449c4e42e4844ad570a97ee2b0aa5b7
Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>
2018-03-20 12:56:45 +0200

    Android.mk: Updated build rules due changes in build system
    For more details please look at:
    https://android.googlesource.com/platform/build/+/master/Changes.md
    
    Change-Id: I98431dfc4ad1b17053a2d5cd23ac1828d88e6fc9
    Signed-off-by: Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>

--- Android.mk
+++ Android.mk
@@ -1,26 +1,47 @@
-OPTEE_SRC=$(ANDROID_BUILD_TOP)/device/renesas/bootloaders/optee/
-export OPTEE_OUT=$(ANDROID_PRODUCT_OUT)/obj/OPTEE_OBJ
-export CROSS_COMPILE64=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-5.2/bin/aarch64-linux-gnu-
+#
+# Copyright (C) 2011 The Android Open-Source Project
+# Copyright (C) 2018 GlobalLogic
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-PLATFORM=rcar
+# Include only for Renesas ones.
+ifneq (,$(filter $(TARGET_PRODUCT), salvator ulcb kingfisher))
 
-LOCAL_CFLAGS+=-march=armv8-a
-LOCAL_CFLAGS+=-mtune=cortex-a57.cortex-a53
+# Android build top/root directory
+ANDROID_ROOT		:= $(shell cd $(OUT_DIR)/target/product/$(TARGET_PRODUCT) && cd ../../../.. && pwd)
+
+OPTEE_SRC		:= device/renesas/bootloaders/optee
+OPTEE_OUT		:= $(ANDROID_ROOT)/$(OUT_DIR)/target/product/$(TARGET_PRODUCT)/obj/OPTEE_OBJ
+OPTEE_CROSS_COMPILE	:= $(ANDROID_ROOT)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-gnu-5.2/bin/aarch64-linux-gnu-
+
+LOCAL_CFLAGS		+= -march=armv8-a
+LOCAL_CFLAGS		+= -mtune=cortex-a57.cortex-a53
 
 $(OPTEE_OUT):
 	$(hide) mkdir -p $(OPTEE_OUT)
 
 optee: $(OPTEE_OUT)
 	@echo "Building optee"
-	$(hide) ARCH=arm make  -e MAKEFLAGS= CFG_ARM64_core=y PLATFORM=rcar -C $(OPTEE_SRC) O=$(OPTEE_OUT) clean
-	$(hide) ARCH=arm make  -e MAKEFLAGS= CFG_ARM64_core=y PLATFORM=rcar -C $(OPTEE_SRC) O=$(OPTEE_OUT) all
-	$(hide) cp $(OPTEE_OUT)/core/*.srec $(ANDROID_PRODUCT_OUT)/
-	$(hide) cp $(OPTEE_OUT)/core/*.bin $(ANDROID_PRODUCT_OUT)/
+	$(hide) ARCH=arm make -e MAKEFLAGS= CFG_ARM64_core=y PLATFORM=rcar -C $(OPTEE_SRC) O=$(OPTEE_OUT) CROSS_COMPILE64=$(OPTEE_CROSS_COMPILE) clean
+	$(hide) ARCH=arm make -e MAKEFLAGS= CFG_ARM64_core=y PLATFORM=rcar -C $(OPTEE_SRC) O=$(OPTEE_OUT) CROSS_COMPILE64=$(OPTEE_CROSS_COMPILE) all
+	$(hide) cp $(OPTEE_OUT)/core/*.srec $(PRODUCT_OUT)/
+	$(hide) cp $(OPTEE_OUT)/core/*.bin $(PRODUCT_OUT)/
 
 android_optee: $(OPTEE_OUT)
 	@echo "Building optee"
-	$(hide) ARCH=arm make  -e MAKEFLAGS= CFG_ARM64_core=y PLATFORM=rcar -C $(OPTEE_SRC) O=$(OPTEE_OUT) clean
-	$(hide) ARCH=arm make  -e MAKEFLAGS= CFG_ARM64_core=y PLATFORM=rcar -C $(OPTEE_SRC) O=$(OPTEE_OUT) all
+	$(hide) ARCH=arm make -e MAKEFLAGS= CFG_ARM64_core=y PLATFORM=rcar -C $(OPTEE_SRC) O=$(OPTEE_OUT) CROSS_COMPILE64=$(OPTEE_CROSS_COMPILE) clean
+	$(hide) ARCH=arm make -e MAKEFLAGS= CFG_ARM64_core=y PLATFORM=rcar -C $(OPTEE_SRC) O=$(OPTEE_OUT) CROSS_COMPILE64=$(OPTEE_CROSS_COMPILE) all
 
 .PHONY: optee
 
@@ -33,7 +54,7 @@ $(TEE_BIN_PATH): android_optee
 
 LOCAL_MODULE := tee.bin
 LOCAL_PREBUILT_MODULE_FILE:= $(TEE_BIN_PATH)
-LOCAL_MODULE_PATH := $(ANDROID_PRODUCT_OUT)
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)
 
 include $(BUILD_EXECUTABLE)
 
@@ -45,6 +66,8 @@ $(TEE_SREC_PATH): android_optee
 
 LOCAL_MODULE := tee.srec
 LOCAL_PREBUILT_MODULE_FILE:= $(TEE_SREC_PATH)
-LOCAL_MODULE_PATH := $(ANDROID_PRODUCT_OUT)
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)
 
 include $(BUILD_EXECUTABLE)
+
+endif # TARGET_PRODUCT salvator ulcb kingfisher
